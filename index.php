ecdb6209ddf53466cb6c9765797a745844d7c9ed
vagrant <vagrant@homestead>
2016-07-06 21:15:50 +0000

    html formatting

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 
-<html lang="en">
+<html lang="en-US">
     <head>
         <title>Simple Task Manager</title>
+        <meta charset="UTF-8">
         <!-- TODO: Add external stylesheets and other dependencies here -->
     </head>
 
     <body>
- 
+        <nav>
+            
+        </nav>
+        <!-- TODO: Create our Task form  -->
+
+        <!-- TODO: Display current tasks -->
+
+        <footer>
+
+        </footer>
     </body>
-
-
 </html>
