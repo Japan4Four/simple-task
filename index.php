562dd746284f55d9d80a7735d253e9ab52543748
vagrant <vagrant@homestead>
2016-07-06 19:59:28 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>Simple Task Manager</title>
+        <!-- TODO: Add external stylesheets and other dependencies here -->
+    </head>
+
+    <body>
+ 
+    </body>
+
+
+</html>
